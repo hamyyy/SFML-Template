5cbe6c1756fc938c6eb03ab3b63a6066021d2eeb
Andrew King <pepsiotaku@gmail.com>
2018-09-01 13:04:35 -0700

    OSX settings

---
+++ env/osx.all.mk
@@ -0,0 +1,22 @@
+CC=g++
+CFLAGS_ALL=-Wfatal-errors -Wextra -Wall -fdiagnostics-color=never
+
+LIB_DIRS= \
+	/usr/local/lib
+
+INCLUDE_DIRS= \
+	/usr/local/include
+
+BUILD_DEPENDENCIES=
+
+PRODUCTION_DEPENDENCIES= \
+	content
+
+PRODUCTION_EXCLUDE= \
+	*.psd \
+	*.rar \
+	*.7z \
+	Thumbs.db \
+	.DS_Store
+
+PRODUCTION_FOLDER=build
---
+++ env/osx.debug.mk
@@ -0,0 +1,7 @@
+CFLAGS=-g $(CFLAGS_ALL) -pg
+
+BUILD_FLAGS= \
+	-pg
+
+BUILD_MACROS= \
+	_DEBUG
