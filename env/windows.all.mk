140973dccc8c86065bcfc031a568380ce1165e9e
Andrew King <pepsiotaku@gmail.com>
2018-10-28 12:52:40 -0700

    folder shortcut variables

--- env/windows.all.mk
+++ env/windows.all.mk
@@ -2,23 +2,27 @@ CC := i686-w64-mingw32-g++.exe
 CFLAGS := -Wfatal-errors -Wunreachable-code -Wextra -Wall -std=c++17
 RC := windres.exe
 
+_MINGW := C:/mingw32/bin
+_SFML := C:/SFML-2.5.1
+_SFML_BIN := $(_SFML)/bin
+
 LIB_DIRS := \
-	C:/SFML-2.5.1/lib
+	$(_SFML)/lib
 
 INCLUDE_DIRS := \
-	C:/SFML-2.5.1/include
+	$(_SFML)/include
 
 BUILD_DEPENDENCIES := \
-	C:/SFML-2.5.1/bin/openal32.dll
+	$(_SFML_BIN)/openal32.dll
 
 PRODUCTION_DEPENDENCIES := \
 	$(PRODUCTION_DEPENDENCIES) \
-	C:/mingw32/bin/libgcc_s_dw2-1.dll \
-	C:/mingw32/bin/libstdc++-6.dll \
-	C:/mingw32/bin/libwinpthread-1.dll \
-	C:/SFML-2.5.1/bin/openal32.dll \
-	C:/SFML-2.5.1/bin/sfml-audio-2.dll \
-	C:/SFML-2.5.1/bin/sfml-graphics-2.dll \
-	C:/SFML-2.5.1/bin/sfml-network-2.dll \
-	C:/SFML-2.5.1/bin/sfml-system-2.dll \
-	C:/SFML-2.5.1/bin/sfml-window-2.dll
+	$(_MINGW)/libgcc_s_dw2-1.dll \
+	$(_MINGW)/libstdc++-6.dll \
+	$(_MINGW)/libwinpthread-1.dll \
+	$(_SFML_BIN)/openal32.dll \
+	$(_SFML_BIN)/sfml-audio-2.dll \
+	$(_SFML_BIN)/sfml-graphics-2.dll \
+	$(_SFML_BIN)/sfml-network-2.dll \
+	$(_SFML_BIN)/sfml-system-2.dll \
+	$(_SFML_BIN)/sfml-window-2.dll
