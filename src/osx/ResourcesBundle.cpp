85e8989e63406056e7335e32c16955455d1b97ef
Andrew King <pepsiotaku@gmail.com>
2018-11-21 00:25:10 -0800

    some comments & minor refactoring

--- src/osx/ResourcesBundle.cpp
+++ src/osx/ResourcesBundle.cpp
@@ -3,19 +3,21 @@
 void initBundle()
 {
 #ifdef __APPLE__
+    // This function ensures the working directory is set inside of the bundle if in production mode
     CFBundleRef mainBundle = CFBundleGetMainBundle();
     CFURLRef resourcesURL = CFBundleCopyResourcesDirectoryURL(mainBundle);
     char path[PATH_MAX];
-    if (!CFURLGetFileSystemRepresentation(resourcesURL, TRUE, (UInt8 *)path, PATH_MAX))
-    {
-        // error!
-    }
+    bool pathSet = CFURLGetFileSystemRepresentation(resourcesURL, TRUE, (UInt8 *)path, PATH_MAX);
+    // This is a copy, so we release it here
     CFRelease(resourcesURL);
 
-    std::string pathStr = path;
-    std::size_t found = pathStr.find(".app");
-    if (found != std::string::npos)
-        chdir(path);
+    // Actually do the check here
+    if (pathSet)
+    {
+        std::string pathStr = path;
+        if (pathStr.find(".app") != std::string::npos)
+            chdir(path);
+    }
 
 #endif
 }
