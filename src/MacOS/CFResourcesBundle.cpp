8fb22ec8de5a420852d861d24d2d841b07d27356
Andrew King <pepsiotaku@gmail.com>
2018-12-02 21:25:15 -0800

    took out extra braces that I shouldn't have added

--- src/MacOS/CFResourcesBundle.cpp
+++ src/MacOS/CFResourcesBundle.cpp
@@ -5,13 +5,11 @@ CFResourcesBundle::CFResourcesBundle()
 {
     // This function ensures the working directory is set inside of the bundle if in production mode
     CFBundleRef mainBundle = CFBundleGetMainBundle();
-    {
-        CFURLRef resourcesURL = CFBundleCopyResourcesDirectoryURL(mainBundle);
-        char path[PATH_MAX];
-        bool pathSet = CFURLGetFileSystemRepresentation(resourcesURL, TRUE, (UInt8 *)path, PATH_MAX);
-        // This is a copy, so we release it here
-        CFRelease(resourcesURL);
-    }
+    CFURLRef resourcesURL = CFBundleCopyResourcesDirectoryURL(mainBundle);
+    char path[PATH_MAX];
+    bool pathSet = CFURLGetFileSystemRepresentation(resourcesURL, TRUE, (UInt8 *)path, PATH_MAX);
+    // This is a copy, so we release it here
+    CFRelease(resourcesURL);
 
     // Actually do the check here
     if (pathSet)
