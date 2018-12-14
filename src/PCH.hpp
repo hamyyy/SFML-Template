a931d19f37641e7c2ceabca22f7f66f1c525bef3
Andrew King <aking@on1.com>
2018-12-14 15:39:58 -0800

    Added SFML to includePath on mac (whoops)

--- src/PCH.hpp
+++ src/PCH.hpp
@@ -1,6 +1,8 @@
 #ifndef PRECOMPILED_HEADER_HPP
 #define PRECOMPILED_HEADER_HPP
 
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
 #include <SFML/Network.hpp>
