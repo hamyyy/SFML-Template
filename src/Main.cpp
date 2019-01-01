a18d12d59fb85fa83343640eda6ccd9c08558cdb
Andrew King <pepsiotaku@gmail.com>
2018-12-31 18:21:42 -0800

    made default window smaller again

--- src/Main.cpp
+++ src/Main.cpp
@@ -4,8 +4,8 @@ int main()
 {
     std::cout << "Hello World!" << std::endl;
 
-    sf::RenderWindow window(sf::VideoMode(400, 400), "SFML works!");
-    sf::CircleShape shape(200.f);
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(window.getSize().x/2);
 
     shape.setFillColor(sf::Color::White);
 
