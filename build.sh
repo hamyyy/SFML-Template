fa1d791f170503f0edaab672c12ddc9f62ebd468
Andrew King <aking@on1.com>
2018-12-31 16:56:54 -0800

    Moved 'nocode' script into build.sh instead

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 # echo $1
 # echo $2
 
+CMD=$1
 BUILD=$2
 
 if [[ $OSTYPE == "linux-gnu" || $OSTYPE == "cygwin" ]]; then
@@ -72,7 +73,25 @@ prod_osx() {
 }
 
 tput setaf 4
-if [[ $1 == 'buildrun' ]] ; then
+if [[ $CMD == 'nocode' ]] ; then
+	CMD=buildprod
+	BUILD=Release
+
+	cwd=${PWD##*/}
+	export PROF_EXEC=gprof
+
+	if [[ $PLATFORM == 'windows' ]]; then
+		export PATH="$PATH:/c/mingw32/bin:/c/SFML-2.5.1/bin"
+		export NAME=$cwd.exe
+		export MAKE_EXEC=mingw32-make
+	elif [[ $PLATFORM == 'linux' || $PLATFORM == 'osx' ]]; then
+		export PATH="/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
+		export NAME=$cwd
+		export MAKE_EXEC=make
+	fi
+fi
+
+if [[ $CMD == 'buildrun' ]] ; then
 	if $MAKE_EXEC BUILD=$BUILD; then
 		build_success_launch
 		bin/$BUILD/$NAME
@@ -80,25 +99,25 @@ if [[ $1 == 'buildrun' ]] ; then
 		build_fail
 	fi
 
-elif [[ $1 == 'build' ]] ; then
+elif [[ $CMD == 'build' ]] ; then
 	if $MAKE_EXEC BUILD=$BUILD; then
 		build_success
 	else
 		build_fail
 	fi
 
-elif [[ $1 == 'rebuild' ]] ; then
+elif [[ $CMD == 'rebuild' ]] ; then
 	if $MAKE_EXEC BUILD=$BUILD rebuild; then
 		build_success
 	else
 		build_fail
 	fi
 
-elif [[ $1 == 'run' ]] ; then
+elif [[ $CMD == 'run' ]] ; then
 	launch
 	bin/$BUILD/$NAME
 
-elif [[ $1 == 'buildprod' ]] ; then
+elif [[ $CMD == 'buildprod' ]] ; then
 	if $MAKE_EXEC BUILD=$BUILD buildprod; then
 		build_success
 	else
@@ -106,7 +125,7 @@ elif [[ $1 == 'buildprod' ]] ; then
 	fi
 
 
-elif [[ $1 == 'profile' ]] ; then
+elif [[ $CMD == 'profile' ]] ; then
 	if [[ $BUILD == 'Debug' ]] ; then
 		if $MAKE_EXEC BUILD=$BUILD; then
 			build_success_launch
@@ -123,7 +142,10 @@ elif [[ $1 == 'profile' ]] ; then
 	fi
 
 else
-	echo Command not recognized
+	tput setaf 1
+	tput bold
+	echo $dec Error: Command \"$CMD\" not recognized. $dec
+	tput sgr0
 fi
 
 tput sgr0
